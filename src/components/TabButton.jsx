61075affd2655b712b0741a5248df22e3b8919e6
Nihillux <oan.mkoll@gmail.com>
2025-03-25 03:22:06 +0100

    last changes

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,10 @@
-export default function TabButton({ children, onSelect }) {
-console.log('TAPBUTTON COMPONENT RENDERED');
-    
+export default function TabButton({ children, isSelected, ...props }) {
+    console.log('TABBUTTON COMPONENT EXECUTING');
     return (
-    <li>
-        <button onClick={onSelect}>{children}</button>
-    </li>
+      <li>
+        <button className={isSelected ? 'active' : undefined} {...props}>
+          {children}
+        </button>
+      </li>
     );
-}
+  }  
