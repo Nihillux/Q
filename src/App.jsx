0675aa45c3d6292f0d82ab430d5de324fb2b24f2
Nihillux <oan.mkoll@gmail.com>
2025-05-10 23:11:40 +0200

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { useState, Fragment } from 'react';
-import Header from './components/Header/Header.jsx';
-import CoreConcepts from './components/CoreConcepts.jsx';
-import Examples from './components/Examples.jsx';
+
 
 function App() {
   return (
-    <Fragment>
-      <Header />
-      <main>
-        <CoreConcepts />
-        <Examples />
-      </main>
-    </Fragment>
+    <p>HELLO</p>
   );
 }
 
